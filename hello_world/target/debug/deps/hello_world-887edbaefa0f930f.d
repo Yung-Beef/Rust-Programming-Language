e7da2b8b92b93c5227966c16df88a6561228382c
Yung-Beef <yungbeefbigbags@gmail.com>
2024-06-20 13:52:00 -0300

    hello world

---
+++ hello_world/target/debug/deps/hello_world-887edbaefa0f930f.d
@@ -0,0 +1,5 @@
+C:\Users\TGang\rust\hello_world\target\debug\deps\libhello_world-887edbaefa0f930f.rmeta: src/main.rs
+
+C:\Users\TGang\rust\hello_world\target\debug\deps\hello_world-887edbaefa0f930f.d: src/main.rs
+
+src/main.rs:
