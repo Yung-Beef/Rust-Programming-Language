4a21a7df7455ec090e1d9d9f89f8de6587c93b88
Yung-Beef <yungbeefbigbags@gmail.com>
2024-07-31 17:45:43 +0200

    refactored employees project

--- projects/employees/src/lib.rs
+++ projects/employees/src/lib.rs
@@ -1,22 +1,43 @@
 use std::{process, io};
 use std::collections::HashMap;
 
+pub fn input() -> (String, bool) {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Unable to read input.");
+    input = input.trim().to_string();
+
+    if input.is_empty() {
+        return (input, false)
+    }
+
+    // only accept alphabetical inputs
+    for letter in input.chars() {
+        if !letter.is_alphabetic() {
+            return (input, false)
+        }
+    }
+
+    return (input, true)
+}
+
 pub fn employees() {
     // let's users select to add a new employee to a department, view existing employees in the company or by department, or quit the program.
 
     let mut map: HashMap<String, String> = HashMap::new();
     
-    loop {
-        let mut input = String::new();
-    
-        println!("\nWould you like to 'Add employee', 'View employees', or 'Quit'?");
-        io::stdin().read_line(&mut input).expect("Unable to read input.");
-        input = input.trim().to_string();
+    'employees: loop {
+        println!("\nWould you like to 'Add' an employee, 'View' the company's employees, or 'Quit'?");
+        let (input, b) = input();
+
+        // if input was empty or contained non-alphabeticals
+        if b == false {
+            continue 'employees
+        }
 
         // matches the user's input and calls the corresponding function
         match input.as_str() {
-            "Add employee" => add_employee(&mut map),
-            "View employees" => view_employees(&map),
+            "Add" => add_employee(&mut map),
+            "View" => view_employees(&map),
             "Quit" => process::exit(0),
             _ => continue,
         }
@@ -26,13 +47,14 @@ pub fn employees() {
 
 pub fn add_employee(map: &mut HashMap<String, String>) {
     // gets the name of the new employee
-    let mut name: String;
-    'name: loop {
-        name = String::new();
-        
+    let name = 'name: loop {
         println!("\nPlease enter the name of the new employee, go 'Back', or 'Quit'.");
-        io::stdin().read_line(&mut name).expect("Unable to read input.");
-        name = name.trim().to_string();
+        let (name, b) = input();
+
+        // if input was empty or contained non-alphabeticals
+        if b == false {
+            continue 'name
+        }
 
         match name.as_str() {
             "Back" => return,
@@ -40,28 +62,19 @@ pub fn add_employee(map: &mut HashMap<String, String>) {
             _ => (),
         }
 
-        // don't accept empty inputs
-        if name.is_empty() {
-            continue 'name
-        }
-
-        // only accept alphabetical inputs
-        for letter in name.chars() {
-            if !letter.is_alphabetic() {
-                continue 'name
-            }
-        }
-        break
-    }
+        break name;
+    };
 
     // gets the department of the new employee
-    let mut department: String;
-    'department: loop {
-        department = String::new();
+    let department = 'department: loop {
         
         println!("\nPlease enter the department of the new employee, go 'Back', or 'Quit'.");
-        io::stdin().read_line(&mut department).expect("Unable to read input.");
-        department = department.trim().to_string();
+        let (department, b) = input();
+
+        // if input was empty or contained non-alphabeticals
+        if b == false {
+            continue 'department
+        }
 
         match department.as_str() {
             "Back" => return,
@@ -69,19 +82,8 @@ pub fn add_employee(map: &mut HashMap<String, String>) {
             _ => (),
         }
 
-        // don't accept empty inputs
-        if department.is_empty() {
-            continue 'department
-        }
-
-        // only accept alphabetical inputs
-        for letter in department.chars() {
-            if !letter.is_alphabetic() {
-                continue 'department
-            }
-        }
-        break
-    }
+        break department;
+    };
 
     // adds employee to the hashmap if they don't exist yet, or updates the employee's department
     map.insert(name, department);
@@ -91,22 +93,14 @@ pub fn add_employee(map: &mut HashMap<String, String>) {
 pub fn view_employees(map: &HashMap<String, String>) {
     // in company or specific department? get input
     'view: loop {
-        let mut input = String::new();
     
         println!("\nWould you like to view employees in the 'Company' or in a specific 'Department'? You can also go 'Back' or 'Quit' the program.");
-        io::stdin().read_line(&mut input).expect("Unable to read input.");
-        input = input.trim().to_string();
 
-        // don't accept empty inputs
-        if input.is_empty() {
+        let (input, b) = input();
+
+        // if input was empty or contained non-alphabeticals
+        if b == false {
             continue 'view
-        }
-
-        // only accept alphabetical inputs
-        for letter in input.chars() {
-            if !letter.is_alphabetic() {
-                continue 'view
-            }
         }
             
         match input.as_str() {
@@ -116,7 +110,7 @@ pub fn view_employees(map: &HashMap<String, String>) {
             "Quit" => process::exit(0),
             _ => continue,
         }
-    }
+    };
 }
 
 pub fn view_company(map: &HashMap<String, String>) {
@@ -139,22 +133,13 @@ pub fn view_company(map: &HashMap<String, String>) {
 pub fn view_department(map: &HashMap<String, String>) {
     // adds all employees from that department from the hashmap to a vector, sorts it, and prints the names
     'view: loop {
-        let mut input = String::new();
     
         println!("\nFor which department would you like to see the employees? You can also go 'Back' or 'Quit'?");
-        io::stdin().read_line(&mut input).expect("Unable to read input.");
-        input = input.trim().to_string();
+        let (input, b) = input();
 
-        // don't accept empty inputs
-        if input.is_empty() {
+        // if input was empty or contained non-alphabeticals
+        if b == false {
             continue 'view
-        }
-
-        // only accept alphabetical inputs
-        for letter in input.chars() {
-            if !letter.is_alphabetic() {
-                continue 'view
-            }
         }
 
         match input.as_str() {
