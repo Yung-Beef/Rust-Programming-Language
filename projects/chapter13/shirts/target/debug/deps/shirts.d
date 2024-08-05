7eecdfddd85c1fbbb09454b880540783717d51a7
Yung-Beef <yungbeefbigbags@gmail.com>
2024-08-05 14:18:21 +0200

    chapter 13

---
+++ projects/chapter13/shirts/target/debug/deps/shirts.d
@@ -0,0 +1,5 @@
+C:\Users\TGang\rust\projects\chapter13\shirts\target\debug\deps\shirts.exe: src/main.rs
+
+C:\Users\TGang\rust\projects\chapter13\shirts\target\debug\deps\shirts.d: src/main.rs
+
+src/main.rs:
---
+++ projects/chapter13/shirts/target/debug/shirts.d
@@ -0,0 +1,1 @@
+C:\Users\TGang\rust\projects\chapter13\shirts\target\debug\shirts.exe: C:\Users\TGang\rust\projects\chapter13\shirts\src\main.rs
