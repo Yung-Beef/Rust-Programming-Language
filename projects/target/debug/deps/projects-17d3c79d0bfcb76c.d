9e4141ede0081e9ed5aac6a06b1f673e117a5602
Yung-Beef <yungbeefbigbags@gmail.com>
2024-08-13 21:40:26 +0300

    reeeeeorganized

--- projects/target/debug/deps/projects-17d3c79d0bfcb76c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\TGang\rust\projects\target\debug\deps\libprojects-17d3c79d0bfcb76c.rmeta: src/main.rs
-
-C:\Users\TGang\rust\projects\target\debug\deps\projects-17d3c79d0bfcb76c.d: src/main.rs
-
-src/main.rs:
--- projects/target/debug/deps/projects-a6d048bd016e8df4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\TGang\rust\projects\target\debug\deps\libprojects-a6d048bd016e8df4.rmeta: src/main.rs
-
-C:\Users\TGang\rust\projects\target\debug\deps\projects-a6d048bd016e8df4.d: src/main.rs
-
-src/main.rs:
